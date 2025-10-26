cdc6645bfade89a3b84ca0a4085306e62ae71ed8
deennii14 <suciudenisa11@gmail.com>
2025-10-26 18:43:23 +0200

    added migration

--- Models/Book.cs
+++ Models/Book.cs
@@ -7,11 +7,16 @@ namespace Suciu_Denisa_Labr2.Models
     {
         public int ID { get; set; }
         [Display(Name = "Book Title")]
-        public string Title { get; set; }
-        public string Author { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
         [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
         [DataType(DataType.Date)]
         public DateTime PublishingDate { get; set; }
+
+        public int? PublisherID { get; set; }
+        public Publisher? Publisher { get; set; }
+
+        public ICollection<BookCategory>? BookCategories { get; set; }
     }
 }
---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Suciu_Denisa_Labr2.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string CategoryName { get; set; }
+        public ICollection<BookCategory>? BookCategories { get; set; }
+    }
+}
